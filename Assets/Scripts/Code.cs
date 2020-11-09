47e501abdae981514e923ac7bedeaf4d97ab289d
Nikita Gribuska <s5065617@bournemouth.ac.uk>
2020-11-09 15:20:02 +0000

    solution

---
+++ Assets/Scripts/Code.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    private Text text;
+    private List<int> numbers = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void AddNumber(int num)
+    {
+        if (numbers.Count < 8)
+        {
+            numbers.Add(num);
+            ChangeText();
+        }
+    }
+
+    public void DeleteNumber()
+    {
+        if (numbers.Count > 0)
+        {
+            numbers.RemoveAt(numbers.Count - 1);
+            ChangeText();
+        }
+    }
+
+    private void ChangeText()
+    {
+        // Reset the text
+        text.text = "";
+
+        int temp = numbers.Count;
+
+        // Fill the numbers from the list
+        for (int i = 0; i < temp; i++)
+        {
+            text.text += numbers[i] + " ";
+        }
+
+        // If not enough numbers then put placeholders
+        for (int i = temp; i < 8; i++)
+        {
+            text.text += "- ";
+        }
+
+        // Remove extra " " in the end 
+        text.text.Remove(temp);
+    }
+
+    public void ResetCode()
+    {
+        if (numbers.Count > 0)
+        {
+            numbers.Clear();
+            ChangeText();
+        }
+    }
+}
---
+++ Assets/Scripts/VideoProgress.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoProgress : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private VideoPlayer videoPlayer = null; // Video player component
+
+    private Image progress; // Image of the progress bar
+
+    void Awake()
+    {
+        progress = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (videoPlayer.frameCount > 0)
+        {
+            progress.fillAmount = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
+        }
+    }
+
+    // For dragging on the proggress bar
+    public void OnDrag(PointerEventData eventData)
+    {
+        TrySkip(eventData);
+    }
+
+    // For clicking the progress bar
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        TrySkip(eventData);
+    }
+
+    // Check for input on the progress bar
+    private void TrySkip(PointerEventData eventData)
+    {
+        Vector2 localPoint;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            progress.rectTransform, eventData.position, null, out localPoint))
+        {
+            float percent = Mathf.InverseLerp(progress.rectTransform.rect.xMin, progress.rectTransform.rect.xMax, localPoint.x);
+
+            SkipToPercent(percent);
+        }
+    }
+
+    // Rewind/Forward the video
+    private void SkipToPercent(float percent)
+    {
+        float frame = videoPlayer.frameCount * percent;
+        videoPlayer.frame = (long)frame;
+    }
+}
